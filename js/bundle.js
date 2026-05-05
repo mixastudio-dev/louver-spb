1678075089b1a67a3b03ae9e98213efd888a84c2
MIXA STUDIO <info@mixastudio.ru>
2026-05-05 16:37:58 +0500

    Update bundle.js

--- js/bundle.js
+++ js/bundle.js
@@ -240,7 +240,33 @@ window.addEventListener('resize', function() {
   setTimeout(checkAllBlocks, 100);
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  const accordionItems = document.querySelectorAll('.accordion-item');
 
+  if (accordionItems) {
+
+    accordionItems.forEach(item => {
+      const trigger = item.querySelector('.accordion-item-header');
+      const content = item.querySelector('.accordion-item-content');
+
+      trigger.addEventListener('click', function() {
+        const parent = this.parentNode;
+
+        if (parent.classList.contains('active')) {
+          parent.classList.remove('active');
+          content.style.height = '0';
+        } else {
+          document.querySelectorAll('.accordion-item').forEach(child => {
+            child.classList.remove('active');
+            child.querySelector('.accordion-item-content').style.height = '0';
+          });
+          parent.classList.add('active');
+          content.style.height = content.scrollHeight + 'px';
+        }
+      });
+    });
+  }
+});
 
 var swiper1 = new Swiper(".directions-slider", {
   observer: true,
@@ -379,3 +405,4 @@ var swiper3 = new Swiper(".process-slider", {
     }
   }
 });
+
